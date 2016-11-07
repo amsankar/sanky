3dfcba856b10a03079cf6e73c3ace986be270195
amsankar <amsankar@gmail.com>
2016-11-07 15:57:48 +0530

    u

--- Sanky/src/org/rams/app/Sanky.java
+++ Sanky/src/org/rams/app/Sanky.java
@@ -1,14 +1,105 @@
 package org.rams.app;
 
 import java.io.Console;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Scanner;
 
 public class Sanky {
 
 	public static void main(String[] args) {
 
-		//authendicate();
+		// authendicate();
 
+		MSAccessTest();
+
+	}
+	
+	private static Connection getWHADConnection() {
+		try {
+			Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+			return  DriverManager.getConnection("jdbc:odbc:Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\\amsankar\\db\\workhelper.accdb");
+		} catch (Exception e) { 
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+
+	private static void MSAccessTest() {
+
+		System.out.println("MS Access Test  ");
+
+		// variables
+		Connection connection = null;
+		Statement statement = null;
+		ResultSet resultSet = null;
+
+		// Step 1: Loading or registering Oracle JDBC driver class
+		try {
+			Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+		} catch (ClassNotFoundException cnfex) {
+			System.out.println("Problem in loading MS Access JDBC driver");
+			cnfex.printStackTrace();
+		}
+
+		System.out.println("MS Access Test  2");
+
+		// Step 2: Opening database connection
+		try {
+
+			String msAccessDBName = "C:\\amsankar\\db\\workhelper.accdb";
+			String dbURL = "jdbc:odbc:Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ="+ msAccessDBName + "";
+			
+			System.out.println(dbURL);
+
+			// Step 2.A: Create and get connection using DriverManager class
+			connection = DriverManager.getConnection(dbURL);
+
+			System.out.println("MS Access Test  3");
+
+			// Step 2.B: Creating JDBC Statement
+			statement = connection.createStatement();
+
+			// Step 2.C: Executing SQL & retrieve data into ResultSet
+			resultSet = statement.executeQuery("SELECT * FROM Application");
+
+			System.out.println("ID\tName\t\t\tAge\tMatches");
+			System.out.println("==\t================\t===\t=======");
+
+			// processing returned data and printing into console
+			while (resultSet.next()) {
+				System.out.println(
+						resultSet.getString(1) + "\t" + resultSet.getString(2) + "\t" + resultSet.getString(3));
+			}
+
+		} catch (SQLException sqlex) {
+			sqlex.printStackTrace();
+		} finally {
+
+			// Step 3: Closing database connection
+			try {
+				if (null != connection) {
+
+					// cleanup resources, once after processing
+					resultSet.close();
+					statement.close();
+
+					// and then finally close connection
+					connection.close();
+				}
+			} catch (SQLException sqlex) {
+				sqlex.printStackTrace();
+			}
+		}
+
+	}
+
+	private static void Test() {
 		String firstName = getCmdInputValue("First Name");
 		String lastName = getCmdInputValue("Last Name");
 		String key = getCmdInputValue(" ");
@@ -17,27 +108,26 @@ public class Sanky {
 			System.out.println("Hello  " + firstName + " " + lastName + "!");
 
 		}
-
 	}
 
 	private static void authendicate() {
-		String pass = getCmdPassword(); 
-		
-		if (pass.substring(1, 2).equals("a") && pass.substring(4, 5).equals("a") ) {
-			
-		}else{
+		String pass = getCmdPassword();
+
+		if (pass.substring(1, 2).equals("a") && pass.substring(4, 5).equals("a")) {
+
+		} else {
 			System.exit(0);
 		}
-	     
+
 	}
 
 	private static String getCmdPassword() {
 		Console console = System.console();
-		
-		if (console==null) {
+
+		if (console == null) {
 			return getCmdInputValueOnly();
 		}
-		   
+
 		return new String(console.readPassword());
 	}
 
@@ -46,9 +136,9 @@ public class Sanky {
 		System.out.print("Enter " + inputName + " : ");
 		return new Scanner(System.in).nextLine();
 	}
-	
+
 	private static String getCmdInputValueOnly() {
- 
+
 		return new Scanner(System.in).nextLine();
 	}
 
--- Sany/src/org/ram/web/AppManager.java
+++ Sany/src/org/ram/web/AppManager.java
@@ -1,10 +1,61 @@
 package org.ram.web;
 
+import java.io.*; 
+
 public class AppManager {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println(123);
+
+
+try{  
+	   FileWriter fw=new FileWriter("c://amsankar/githubconfig/abc.txt");  
+	   fw.write("sample file");  
+	   fw.close();  
+	   
+	   
+		  FileReader fr=new FileReader("c://amsankar/githubconfig/config.txt");  
+		  int i;  
+		  while((i=fr.read())!=-1)  
+		  System.out.println((char)i);  
+		  
+		  fr.close();
+	   
+		  
+		  
+	      File file = null;
+	      String[] paths;
+	  
+	      try {      
+	         // create new file object
+	         file = new File("c://amsankar/githubconfig");
+
+	         // array of files and directory
+	         paths = file.list();
+
+	         // for each name in the path array
+	         for(String path:paths) {
+	            // prints filename and directory name
+	            System.out.println(path);
+	         }
+	      }catch(Exception e) {
+	         // if any error occurs
+	         e.printStackTrace();
+	      }
+		  
+		  
+		  
+	   
+	  }catch(Exception e){System.out.println(e);}  
+	  System.out.println("success");  
+	 
+
+	  
+  
+	  
+	  
+
 	}
 
 }
